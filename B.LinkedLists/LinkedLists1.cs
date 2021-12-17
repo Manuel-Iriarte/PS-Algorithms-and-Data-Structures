62051da887913c8919ab215e672321233ae4dd9c
Manuel <iriarte.manuel@yahoo.es>
2021-12-17 11:52:48 -0300

    ...

--- B.LinkedLists/LinkedLists1.cs
+++ B.LinkedLists/LinkedLists1.cs
@@ -1,5 +1,7 @@
 ﻿using Core;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace B.LinkedLists
 {
@@ -24,50 +26,110 @@ namespace B.LinkedLists
         {
             // ejemplo de nodos y su creacion con la clase nodo
 
-            var lista = new ListaEnlazadaDoble<int>();
+            //var lista = new ListaEnlazadaDoble<int>();
 
 
+
+            var listaOriginal = new List<int>();
+
+            listaOriginal.Add(1);
+            listaOriginal.Add(2);
+            listaOriginal.Add(1);
+            listaOriginal.Add(4);
+            listaOriginal.Add(5);
+            listaOriginal.Add(4);
+
+            var result = 0;
+            var listas = new List<List<int>>();
+            int count = 0;
+            int teamsNumber = listaOriginal.Count / 3;
+            var maxVal = listaOriginal.Max(n => n);
+            var minVval = maxVal - 2;
+
+            for (int i = 0; i < 3; i++)
+            {
+                var lista = new List<int>();
+
+                foreach (int sk in listaOriginal)
+                {
+                    if (count != 3 && sk >= minVval && sk <= maxVal)
+                    {
+                        lista.Add(sk);
+                        count++;
+                    }
+                }
+                listas.Add(lista);
+                if (i < teamsNumber)
+                {
+                    maxVal = listaOriginal.Where(n => n < minVval).Max(n => n);
+                    minVval = maxVal - 2;
+                }
+            }
+
+            foreach (var l in listas)
+            {
+                Console.WriteLine(l.Count);
+                foreach (int u in l)
+                {
+                    Console.WriteLine(u);
+                }
+            }
+
+
+
+            //var listas = new List<List<int>>();
+
+
+            //foreach(int skill in listaOriginal)
+            //{
+            //    if(1 <= 9)
+            //    {
+
+            //    }
+            //}
+
+            Console.WriteLine();
             // doubly linked list
             // es como el single pero se puede mover en reversa, teniendo una referencia al nodo anterior
             // la clase nodo (NodoD) contiene una referencia tambien al nodo anterior
 
-            lista.Add(1);
-            lista.Add(3); 
-            lista.Add(5);
-            lista.Add(7);
+            //lista.Add(1);
+            //lista.Add(3); 
+            //lista.Add(5);
+            //lista.Add(7);
 
-            Console.WriteLine("Listar");
+            //Console.WriteLine("Listar");
 
-            foreach (int numero in lista)
-            {
-                Console.WriteLine(numero);
-            }
+            //foreach (int numero in lista)
+            //{
+            //    Console.WriteLine(numero);
+            //}
 
-            Console.WriteLine("Listar Inverso");
+            //Console.WriteLine("Listar Inverso");
 
-            foreach (int numero in lista.GetReverseEnumerator())
-            {
-                Console.WriteLine(numero);
-            }
+            //foreach (int numero in lista.GetReverseEnumerator())
+            //{
+            //    Console.WriteLine(numero);
+            //}
 
-            Console.WriteLine("Contiene valor buscado?");
+            //Console.WriteLine("Contiene valor buscado?");
 
-            Console.WriteLine(lista.Contains(5));
+            //Console.WriteLine(lista.Contains(5));
 
-            Console.WriteLine(lista.Contains(6));
+            //Console.WriteLine(lista.Contains(6));
 
-            Console.WriteLine("Encuentra valor buscado?");
+            //Console.WriteLine("Encuentra valor buscado?");
 
-            Console.WriteLine(lista.Find(3).NodoAnterior.Valor);
+            //Console.WriteLine(lista.Find(3).NodoAnterior.Valor);
 
-            Console.WriteLine("Remueve un valor:");
+            //Console.WriteLine("Remueve un valor:");
 
-            lista.Remove(3);
+            //lista.Remove(3);
 
-            foreach (int numero in lista)
-            {
-                Console.WriteLine(numero);
-            }
+            //foreach (int numero in lista)
+            //{
+            //    Console.WriteLine(numero);
+            //}
         }
     }
 }
